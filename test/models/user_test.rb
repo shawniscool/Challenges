c9f139a3bb67b2b5b7f0eeff8d9f83e1bee95fbe
shawniscool <xiangpan@Jun-Zhengs-MacBook-Pro.local>
2015-03-28 15:50:42 -0400

    commit works

---
+++ test/models/user_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+end
