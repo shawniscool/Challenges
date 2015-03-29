e485956dd89c3126a662ee3f785293d4201866a6
shawniscool <xiangpan@Jun-Zhengs-MacBook-Pro.local>
2015-03-29 02:39:41 -0400

    relationship not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :challenges
   has_many :relationships, class_name: "Relationship", 
               foreign_key: "user_id",dependent: :destroy
+
+
 	before_save { self.email = email.downcase }
 	validates :name,  presence: true, length: { maximum: 50 }
   	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
